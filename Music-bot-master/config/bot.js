5e192219b80e910be10dfd8d19fda7ab8aff2ac5
caist <caist@hectic.email>
2021-05-29 22:26:53 -0500

    Add files via upload

---
+++ Music-bot-master/config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODQ4NDAwODk3ODA0NTk5Mzc3.YLME6A.lcorXrngz_rT9Lq1KD1-r7tBTC4',
+        prefix: '!',
+        activity: 'To Music',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
